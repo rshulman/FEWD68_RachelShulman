60d33e04fd3ae66a35b4443d1b32f52aba2879a1
Rachel Shulman <rachelshulman@Rachels-MacBook-Pro.local>
2015-11-23 18:19:26 -0500

    week  9 hw

---
+++ Refactor Labs/color_scheme/js/index.js
@@ -0,0 +1,30 @@
+//$('#grayButton').click(switchGray);
+//$('#whiteButton').click(switchWhite);
+//$('#blueButton').click(switchBlue);
+//$('#yellowButton').click(switchYellow);
+
+//function switchGray() {
+//  $('body').attr('class', 'gray');
+//}
+
+//function switchWhite() {
+//  $('body').attr('class', 'white');
+//}
+
+//function switchBlue() {
+ // $('body').attr('class', 'blue');
+//}
+
+//function switchYellow() {
+//  $('body').attr('class', 'yellow');
+//}
+
+function switchTheme() {
+  var theme = $(this).attr("class");
+  $("body").attr("class", theme);
+}
+
+$(document).ready(function(){
+  $("#switcher li").on("click", switchTheme);
+
+});
